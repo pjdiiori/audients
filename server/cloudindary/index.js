4d29761d5f894d3bdbeae3d390fb48c42e3ef441
PJ DiIorio <pjdiiori@gmail.com>
2021-10-02 22:50:59 -0500

    formatting

--- server/cloudindary/index.js
+++ server/cloudindary/index.js
@@ -1,22 +1,22 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        resource_type: "auto",
-        folder: 'audients',
-        allowed_formats: ['mp3', 'wav', 'txt', 'jpeg', 'jpg', 'png']
-    }
+  cloudinary: cloudinary,
+  params: {
+    resource_type: "auto",
+    folder: "audients",
+    allowed_formats: ["mp3", "wav", "txt", "jpeg", "jpg", "png"],
+  },
 });
 
 module.exports = {
-    cloudinary,
-    storage
-}
+  cloudinary,
+  storage,
+};
